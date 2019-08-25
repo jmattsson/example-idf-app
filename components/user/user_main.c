56e4486dc12ceba19dccdf0e2f51a6f16a6225b3
Johny Mattsson <johny.mattsson+github@gmail.com>
2019-08-25 17:16:12 +1000

    Submodule the nonos-sdk-idf, misc updates.

--- components/user/user_main.c
+++ components/user/user_main.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "user_interface.h"
+#include <stdint.h>
 
 int ets_printf(const char *format, ...)  __attribute__ ((format (printf, 1, 2)));
 
@@ -10,7 +11,8 @@ void user_pre_init(void)
 
 void user_init(void)
 {
-  ets_printf("Hello, esp8266!\n");
+  uint32_t heap = system_get_free_heap_size();
+  ets_printf("Hello, esp8266!\nFree heap is %u\n", heap);
   for(;;)
     ;
 }
